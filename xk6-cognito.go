f9699681c0ff0b2c746a9cdad9c68cc7d934ce21
AjayvirS <38434017+AjayvirS@users.noreply.github.com>
2024-02-27 15:15:05 +0100

    Update xk6-cognito.go

--- xk6-cognito.go
+++ xk6-cognito.go
@@ -54,7 +54,7 @@ func contains(array []string, element string) bool {
 	return false
 }
 
-func (r *Cognito) Connect(ctx context.Context, region string ) (*Client, error) {
+func (r *Cognito) Connect(region string ) (*Client, error) {
 
 	regionAws := config.WithRegion(region)
 	// cred := config.WithCredentialsProvider(aws.AnonymousCredentials{})
@@ -71,7 +71,7 @@ func (r *Cognito) Connect(ctx context.Context, region string ) (*Client, error) 
 	}
 
 	client := Client{
-		ctx:           ctx,
+		ctx:           context.TODO(),
 		client: cip.NewFromConfig(cfg),
 		
 	}
